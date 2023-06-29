3feb01b04c561e8216cd1bb1969cd4479b12d182
ng1n3 <code.with.muyiwa@gmail.com>
2023-06-29 14:48:41 +0100

    working on drop down notification shade

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-'use strict';
+"use strict";
+
+const buttonwrapper = document.querySelector(".button-wrapper");
+const asideoption = document.querySelector('.aside-option');
+const checkbox = document.querySelector('#checkbox');
+
+console.log(checkbox.checked);
